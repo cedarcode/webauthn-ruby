5581661cf02eca746822ff39dfdf49058e226dda
Gonzalo Rodriguez <gonzalo@cedarcode.com>
2018-05-15 11:38:50 -0300

    test: group test cases

--- spec/webauthn_spec.rb
+++ spec/webauthn_spec.rb
@@ -3,31 +3,29 @@ RSpec.describe WebAuthn do
     expect(WebAuthn::VERSION).not_to be nil
   end
 
-  it "has a 16 byte length challenge" do
-    payload = WebAuthn.registration_payload
+  describe "#registration_payload" do
+    before do
+      @payload = WebAuthn.registration_payload
+    end
 
-    expect(payload[:publicKey][:challenge].length).to eq(16)
-  end
+    it "has a 16 byte length challenge" do
+      expect(@payload[:publicKey][:challenge].length).to eq(16)
+    end
 
-  it "has public key params" do
-    payload = WebAuthn.registration_payload
+    it "has public key params" do
+      expect(@payload[:publicKey][:pubKeyCredParams][0][:type]).to eq("public-key")
+      expect(@payload[:publicKey][:pubKeyCredParams][0][:alg]).to eq(-7)
+    end
 
-    expect(payload[:publicKey][:pubKeyCredParams][0][:type]).to eq("public-key")
-    expect(payload[:publicKey][:pubKeyCredParams][0][:alg]).to eq(-7)
-  end
+    it "has relying party info" do
+      expect(@payload[:publicKey][:rp][:name]).to eq("web-server")
+    end
 
-  it "has relying party info" do
-    payload = WebAuthn.registration_payload
-
-    expect(payload[:publicKey][:rp][:name]).to eq("web-server")
-  end
-
-  it "has user info" do
-    payload = WebAuthn.registration_payload
-
-    user_info = payload[:publicKey][:user]
-    expect(user_info[:name]).to eq("web-user")
-    expect(user_info[:displayName]).to eq("web-user")
-    expect(user_info[:id]).to eq("MQ==\n")
+    it "has user info" do
+      user_info = @payload[:publicKey][:user]
+      expect(user_info[:name]).to eq("web-user")
+      expect(user_info[:displayName]).to eq("web-user")
+      expect(user_info[:id]).to eq("MQ==\n")
+    end
   end
 end
