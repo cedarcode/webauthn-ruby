3a5fa7c14453e6f952d2b686f073e44a26f3b867
Gonzalo Rodriguez <grzuy0@gmail.com>
2018-05-31 16:55:18 -0300

    test: avoid excessive repetition in test setup

--- spec/webauthn/authenticator_assertion_response_spec.rb
+++ spec/webauthn/authenticator_assertion_response_spec.rb
@@ -7,153 +7,91 @@ RSpec.describe WebAuthn::AuthenticatorAssertionResponse do
   let(:encoded_challenge) { WebAuthn::Utils.ua_encode(challenge) }
   let(:original_origin) { fake_origin }
 
+  let(:client_data_json) { fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get") }
+  let(:encoded_client_data_json) { WebAuthn::Utils.ua_encode(client_data_json) }
+  let(:credential_key) { fake_credential_key }
+  let(:authenticator_data) { fake_authenticator_data(credential_public_key: credential_key.public_key) }
+
+  let(:signature) do
+    fake_signature(
+      key: credential_key,
+      authenticator_data: authenticator_data,
+      client_data_json: client_data_json
+    )
+  end
+
+  let(:assertion_response) do
+    WebAuthn::AuthenticatorAssertionResponse.new(
+      client_data_json: encoded_client_data_json,
+      authenticator_data: authenticator_data,
+      signature: signature
+    )
+  end
+
   it "is valid if everything's in place" do
-    credential_key = fake_credential_key
-    client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get")
-    authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key)
-
-    assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-      client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-      authenticator_data: authenticator_data,
-      signature: fake_signature(
-        key: credential_key,
-        authenticator_data: authenticator_data,
-        client_data_json: client_data_json
-      )
-    )
-
-    is_valid = assertion_response.valid?(
-      encoded_challenge,
-      original_origin,
-      credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
-    )
-
-    expect(is_valid).to be_truthy
-  end
-
-  it "is invalid if signature was signed with a different key" do
-    credential_key = fake_credential_key
-    client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get")
-    authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key)
-
-    assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-      client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-      authenticator_data: authenticator_data,
-      signature: fake_signature(
-        key: credential_key,
-        authenticator_data: authenticator_data,
-        client_data_json: client_data_json
-      )
-    )
-
-    different_key = fake_credential_key
-
-    is_valid = assertion_response.valid?(
-      encoded_challenge,
-      original_origin,
-      credential_public_key: different_key.public_key.to_octet_string(:uncompressed)
-    )
-
-    expect(is_valid).to be_falsy
-  end
-
-  describe "type validation" do
-    it "is invalid if type is not get" do
-      credential_key = fake_credential_key
-      client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.create")
-      authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key)
-
-      assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-        client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-        authenticator_data: authenticator_data,
-        signature: fake_signature(
-          key: credential_key,
-          authenticator_data: authenticator_data,
-          client_data_json: client_data_json
-        )
-      )
-
-      is_valid = assertion_response.valid?(
+    expect(
+      assertion_response.valid?(
         encoded_challenge,
         original_origin,
         credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
       )
+    ).to be_truthy
+  end
 
-      expect(is_valid).to be_falsy
+  it "is invalid if signature was signed with a different key" do
+    different_key = fake_credential_key
+
+    expect(
+      assertion_response.valid?(
+        encoded_challenge,
+        original_origin,
+        credential_public_key: different_key.public_key.to_octet_string(:uncompressed)
+      )
+    ).to be_falsy
+  end
+
+  describe "type validation" do
+    let(:client_data_json) { fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.create") }
+
+    it "is invalid if type is not get" do
+      expect(
+        is_valid = assertion_response.valid?(
+          encoded_challenge,
+          original_origin,
+          credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
+        )
+      ).to be_falsy
     end
   end
 
   describe "user present validation" do
+    let(:authenticator_data) { fake_authenticator_data(credential_public_key: credential_key.public_key, user_present: false) }
+
     it "is invalid if user-present flag is off" do
-      credential_key = fake_credential_key
-      client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get")
-      authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key, user_present: false)
-
-      assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-        client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-        authenticator_data: authenticator_data,
-        signature: fake_signature(
-          key: credential_key,
-          authenticator_data: authenticator_data,
-          client_data_json: client_data_json
+      expect(
+        assertion_response.valid?(
+          encoded_challenge,
+          original_origin,
+          credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
         )
-      )
-
-      is_valid = assertion_response.valid?(
-        encoded_challenge,
-        original_origin,
-        credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
-      )
-
-      expect(is_valid).to be_falsy
+      ).to be_falsy
     end
   end
 
   describe "challenge validation" do
     it "is invalid if challenge doesn't match" do
-      credential_key = fake_credential_key
-      client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get")
-      authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key)
-
-      original_challenge = fake_challenge
-      encoded_original_challenge = WebAuthn::Utils.ua_encode(original_challenge)
-
-      assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-        client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-        authenticator_data: authenticator_data,
-        signature: fake_signature(
-          key: credential_key,
-          authenticator_data: authenticator_data,
-          client_data_json: client_data_json
+      expect(
+        assertion_response.valid?(
+          WebAuthn::Utils.ua_encode(fake_challenge),
+          original_origin,
+          credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
         )
-      )
-
-      is_valid = assertion_response.valid?(
-        encoded_original_challenge,
-        original_origin,
-        credential_public_key: credential_key.public_key.to_octet_string(:uncompressed)
-      )
-
-      expect(is_valid).to be_falsy
+      ).to be_falsy
     end
   end
 
   describe "origin validation" do
     it "is invalid if origin doesn't match" do
-      credential_key = fake_credential_key
-      client_data_json = fake_client_data_json(challenge: challenge, origin: original_origin, type: "webauthn.get")
-      authenticator_data = fake_authenticator_data(credential_public_key: credential_key.public_key)
-
-      assertion_response = WebAuthn::AuthenticatorAssertionResponse.new(
-        client_data_json: WebAuthn::Utils.ua_encode(client_data_json),
-        authenticator_data: authenticator_data,
-        signature: fake_signature(
-          key: credential_key,
-          authenticator_data: authenticator_data,
-          client_data_json: client_data_json
-        )
-      )
-
       expect(
         assertion_response.valid?(
           encoded_challenge,
