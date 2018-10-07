b37d36ecb8563bc1e6411fe12fd7e85b4b922aee
Sorah Fukumori <sorah@cookpad.com>
2018-10-07 19:45:19 +0900

    fixup: FormatNotSupportedError

--- lib/webauthn/attestation_statement.rb
+++ lib/webauthn/attestation_statement.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'webauthn/attestation_statement/base'
-
 module WebAuthn
   module AttestationStatement
+    class FormatNotSupportedError < StandardError; end
+
     ATTESTATION_FORMAT_NONE = "none"
     ATTESTATION_FORMAT_FIDO_U2F = "fido-u2f"
     ATTESTATION_FORMAT_PACKED = 'packed'
@@ -20,7 +20,7 @@ module WebAuthn
         require "webauthn/attestation_statement/packed"
         WebAuthn::AttestationStatement::Packed.new(statement)
       else
-        raise WebAuthn::AttestationStatement::Base::NotSupportedError, "Unsupported attestation format '#{format}'"
+        raise FormatNotSupportedError, "Unsupported attestation format '#{format}'"
       end
     end
   end
