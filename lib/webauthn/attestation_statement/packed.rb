1e6ba274969d5ed134be80f361e9a6d5ac702ee0
Bart de Water <bartdewater@gmail.com>
2019-03-10 20:59:50 -0400

    fix: verify AAGUID match for packed basic attestation

--- lib/webauthn/attestation_statement/packed.rb
+++ lib/webauthn/attestation_statement/packed.rb
@@ -15,6 +15,7 @@ module WebAuthn
         valid_format? &&
           valid_certificate_chain?(authenticator_data.credential) &&
           meet_certificate_requirement? &&
+          matching_aaguid?(authenticator_data.attested_credential_data.aaguid) &&
           valid_signature?(authenticator_data, client_data_hash) &&
           attestation_type_and_trust_path
       end
@@ -74,6 +75,18 @@ module WebAuthn
           attestation_certificate.version == 2 &&
             subject.assoc('OU')&.at(1) == "Authenticator Attestation" &&
             attestation_certificate.extensions.find { |ext| ext.oid == 'basicConstraints' }&.value == 'CA:FALSE'
+        else
+          true
+        end
+      end
+
+      def matching_aaguid?(attested_credential_data_aaguid)
+        extension = attestation_certificate&.extensions&.detect { |ext| ext.oid == AAGUID_EXTENSION_OID }
+        if extension
+          # `extension.value` mangles data into ASCII, so we must manually compare bytes
+          # see https://github.com/ruby/openssl/pull/234
+          extension.to_der[-WebAuthn::AuthenticatorData::AttestedCredentialData::AAGUID_LENGTH..-1] ==
+            attested_credential_data_aaguid
         else
           true
         end
