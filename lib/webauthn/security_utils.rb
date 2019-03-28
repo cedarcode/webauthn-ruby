b2ac9d172ab8432a71a94a083a37995cf1f8be8a
Bart de Water <496367+bdewater@users.noreply.github.com>
2019-03-27 22:40:59 -0400

    Speed up secure compare
    Hex encoding is base 16 which makes the original input twice as big. We don't need to encode the SHA256 output bytes and the shorter output means we spend less time in `SecureCompare.compare`

--- lib/webauthn/security_utils.rb
+++ lib/webauthn/security_utils.rb
@@ -10,8 +10,8 @@ module WebAuthn
     # The values are first processed by SHA256, so that we don't leak length info
     # via timing attacks.
     def secure_compare(first_string, second_string)
-      first_string_sha256 = ::Digest::SHA256.hexdigest(first_string)
-      second_string_sha256 = ::Digest::SHA256.hexdigest(second_string)
+      first_string_sha256 = ::Digest::SHA256.digest(first_string)
+      second_string_sha256 = ::Digest::SHA256.digest(second_string)
 
       SecureCompare.compare(first_string_sha256, second_string_sha256) && first_string == second_string
     end
