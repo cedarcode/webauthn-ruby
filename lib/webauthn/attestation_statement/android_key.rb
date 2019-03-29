5e5632eea6418e7b16d406dadc26c5284f2a2d38
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-29 18:27:46 -0300

    refactor: attempt to improve understandability by creating abstractions around parsing

--- lib/webauthn/attestation_statement/android_key.rb
+++ lib/webauthn/attestation_statement/android_key.rb
@@ -1,20 +1,13 @@
 # frozen_string_literal: true
 
 require "openssl"
+require "webauthn/attestation_statement/android_key/key_description"
 require "webauthn/attestation_statement/base"
 
 module WebAuthn
   module AttestationStatement
     class AndroidKey < Base
       EXTENSION_DATA_OID = "1.3.6.1.4.1.11129.2.1.17"
-
-      # https://developer.android.com/training/articles/security-key-attestation#certificate_schema
-      ATTESTATION_CHALLENGE_INDEX = 4
-      SOFTWARE_ENFORCED_INDEX = 6
-      TEE_ENFORCED_INDEX = 7
-      PURPOSE_TAG = 1
-      ALL_APPLICATIONS_TAG = 600
-      ORIGIN_TAG = 702
 
       # https://android.googlesource.com/platform/hardware/libhardware/+/master/include/hardware/keymaster_defs.h
       KM_ORIGIN_GENERATED = 0
@@ -45,38 +38,35 @@ module WebAuthn
       end
 
       def valid_attestation_challenge?(client_data_hash)
-        WebAuthn::SecurityUtils.secure_compare(key_description[ATTESTATION_CHALLENGE_INDEX].value, client_data_hash)
+        WebAuthn::SecurityUtils.secure_compare(key_description.attestation_challenge, client_data_hash)
       end
 
       def all_applications_field_not_present?
-        tee_enforced.none? { |data| data.tag == ALL_APPLICATIONS_TAG } &&
-          software_enforced.none? { |data| data.tag == ALL_APPLICATIONS_TAG }
+        tee_enforced.all_applications.nil? && software_enforced.all_applications.nil?
       end
 
       def valid_authorization_list_origin?
-        tee_enforced.detect { |data| data.tag == ORIGIN_TAG }&.value&.at(0)&.value == KM_ORIGIN_GENERATED ||
-          software_enforced.detect { |data| data.tag == ORIGIN_TAG }&.value&.at(0)&.value == KM_ORIGIN_GENERATED
+        tee_enforced.origin == KM_ORIGIN_GENERATED || software_enforced.origin == KM_ORIGIN_GENERATED
       end
 
       def valid_authorization_list_purpose?
-        tee_enforced.detect { |data| data.tag == PURPOSE_TAG }&.value&.at(0)&.value&.at(0)&.value == KM_PURPOSE_SIGN ||
-          software_enforced.detect { |data| data.tag == PURPOSE_TAG }&.value&.at(0)&.value&.at(0)&.value ==
-            KM_PURPOSE_SIGN
+        tee_enforced.purpose == KM_PURPOSE_SIGN || software_enforced.purpose == KM_PURPOSE_SIGN
       end
 
       def tee_enforced
-        key_description[SOFTWARE_ENFORCED_INDEX].value
+        key_description.tee_enforced
       end
 
       def software_enforced
-        key_description[TEE_ENFORCED_INDEX].value
+        key_description.software_enforced
       end
 
       def key_description
         @key_description ||= begin
           extension_data = attestation_certificate.extensions.detect { |ext| ext.oid == EXTENSION_DATA_OID }
           raw_key_description = OpenSSL::ASN1.decode(extension_data).value.last
-          OpenSSL::ASN1.decode(raw_key_description.value).value
+
+          KeyDescription.new(OpenSSL::ASN1.decode(raw_key_description.value).value)
         end
       end
 
