cd0a698ab52977fd20edab4289bd2b9ce9416ee0
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-04-20 19:45:01 -0300

    refactor: attempt to improve legibility

--- lib/webauthn/attestation_statement/tpm/pub_area.rb
+++ lib/webauthn/attestation_statement/tpm/pub_area.rb
@@ -56,23 +56,33 @@ module WebAuthn
         end
 
         def valid_ecc_key?(cose_key)
-          t_public.parameters.symmetric == ::TPM::ALG_NULL &&
-            (
-              t_public.parameters.scheme == ::TPM::ALG_NULL ||
-              t_public.parameters.scheme == COSE_ECC_TO_TPM_ALG[cose_key.alg]
-            ) &&
-            t_public.parameters.curve_id == COSE_TO_TPM_CURVE[cose_key.crv] &&
-            t_public.unique.buffer == cose_key.x + cose_key.y
+          valid_symmetric? &&
+            valid_scheme?(COSE_ECC_TO_TPM_ALG[cose_key.alg]) &&
+            parameters.curve_id == COSE_TO_TPM_CURVE[cose_key.crv] &&
+            unique == cose_key.x + cose_key.y
         end
 
         def valid_rsa_key?(cose_key)
-          t_public.parameters.symmetric == ::TPM::ALG_NULL &&
-            (
-              t_public.parameters.scheme == ::TPM::ALG_NULL ||
-              t_public.parameters.scheme == COSE_RSA_TO_TPM_ALG[cose_key.alg]
-            ) &&
-            t_public.parameters.key_bits == cose_key.n.size * BYTE_LENGTH &&
-            t_public.unique.buffer == cose_key.n
+          valid_symmetric? &&
+            valid_scheme?(COSE_RSA_TO_TPM_ALG[cose_key.alg]) &&
+            parameters.key_bits == cose_key.n.size * BYTE_LENGTH &&
+            unique == cose_key.n
+        end
+
+        def valid_symmetric?
+          parameters.symmetric == ::TPM::ALG_NULL
+        end
+
+        def valid_scheme?(scheme)
+          parameters.scheme == ::TPM::ALG_NULL || parameters.scheme == scheme
+        end
+
+        def unique
+          t_public.unique.buffer
+        end
+
+        def parameters
+          t_public.parameters
         end
 
         def t_public
