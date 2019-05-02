ac6a1658c5b13f28f4d225dacc550317fdaeb560
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-05-02 17:31:30 -0300

    refactor: move ATTESTATION_TYPE_* constants to proper context

--- lib/webauthn/attestation_statement.rb
+++ lib/webauthn/attestation_statement.rb
@@ -13,13 +13,6 @@ module WebAuthn
     ATTESTATION_FORMAT_ANDROID_KEY = "android-key"
     ATTESTATION_FORMAT_TPM = "tpm"
 
-    ATTESTATION_TYPE_NONE = "None"
-    ATTESTATION_TYPE_BASIC = "Basic"
-    ATTESTATION_TYPE_SELF = "Self"
-    ATTESTATION_TYPE_ATTCA = "AttCA"
-    ATTESTATION_TYPE_ECDAA = "ECDAA"
-    ATTESTATION_TYPE_BASIC_OR_ATTCA = "Basic_or_AttCA"
-
     def self.from(format, statement)
       case format
       when ATTESTATION_FORMAT_NONE
--- lib/webauthn/attestation_statement/base.rb
+++ lib/webauthn/attestation_statement/base.rb
@@ -6,6 +6,13 @@ require "webauthn/error"
 
 module WebAuthn
   module AttestationStatement
+    ATTESTATION_TYPE_NONE = "None"
+    ATTESTATION_TYPE_BASIC = "Basic"
+    ATTESTATION_TYPE_SELF = "Self"
+    ATTESTATION_TYPE_ATTCA = "AttCA"
+    ATTESTATION_TYPE_ECDAA = "ECDAA"
+    ATTESTATION_TYPE_BASIC_OR_ATTCA = "Basic_or_AttCA"
+
     class Base
       class NotSupportedError < Error; end
 
