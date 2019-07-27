f8e26994ebdbfcd265af94dcfce82eb8e6029e1a
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-07-27 15:06:51 -0300

    test: add coverage for android safetynet attestation certificate trust validation

--- spec/webauthn/attestation_statement/android_safetynet_spec.rb
+++ spec/webauthn/attestation_statement/android_safetynet_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe "android-safetynet attestation" do
         payload,
         attestation_key,
         "ES256",
-        x5c: [Base64.strict_encode64(leaf_certificate)]
+        x5c: [Base64.strict_encode64(leaf_certificate.to_der)]
       )
     end
 
@@ -35,7 +35,7 @@ RSpec.describe "android-safetynet attestation" do
 
       certificate.sign(attestation_key, OpenSSL::Digest::SHA256.new)
 
-      certificate.to_der
+      certificate
     end
 
     let(:authenticator_data) { WebAuthn::AuthenticatorData.new(authenticator_data_bytes) }
@@ -50,13 +50,34 @@ RSpec.describe "android-safetynet attestation" do
     let(:credential_key) { OpenSSL::PKey::EC.new("prime256v1").generate_key }
     let(:client_data_hash) { OpenSSL::Digest::SHA256.digest({}.to_json) }
 
-    before do
-      # TODO: Test attestation cert chain validation
-      allow(statement).to receive(:trusted_attestation_certificate?).and_return(true)
+    let(:trust_store) do
+      trust_store = OpenSSL::X509::Store.new
+      trust_store.add_cert(attestation_certificate)
+
+      trust_store
+    end
+
+    let(:attestation_certificate) do
+      certificate = OpenSSL::X509::Certificate.new
+      certificate.not_before = Time.now
+      certificate.not_after = Time.now + 60
+      certificate.public_key = attestation_key
+
+      certificate.sign(attestation_key, OpenSSL::Digest::SHA256.new)
+
+      certificate
     end
 
     it "returns true when everything's in place" do
-      expect(statement.valid?(authenticator_data, client_data_hash)).to be_truthy
+      expect(statement.valid?(authenticator_data, client_data_hash, trust_store: trust_store)).to be_truthy
+    end
+
+    context "when the attestation certificate is not trusted" do
+      let(:trust_store) { OpenSSL::X509::Store.new }
+
+      it "returns false" do
+        expect(statement.valid?(authenticator_data, client_data_hash, trust_store: trust_store)).to be_falsy
+      end
     end
 
     context "when nonce is not set to the base64 of the SHA256 of authData + clientDataHash" do
