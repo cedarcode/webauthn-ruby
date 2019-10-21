513f9bf5d31dfe136c7d0d5ebcaf3363fcdd136e
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-10-21 14:29:15 -0300

    test: remove unused env var
    This env var is apparently for when using algon with java command.
    
    On top of that it seems we never used the correct env var, which is
    FAKETIME_DONT_FAKE_MONOTONIC instead of DONT_FAKE_MONOTONIC.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -65,7 +65,7 @@ end
 
 # Uses https://github.com/wolfcw/libfaketime/ to stub time. Unlike the Timecop gem this also works for C-extensions
 # such as OpenSSL. The library must be loaded per the libfaketime's README for the relevant operating system, and use
-# the DONT_FAKE_MONOTONIC=1 and FAKETIME_NO_CACHE=1 environment variables to work as expected for the test suite.
+# the FAKETIME_NO_CACHE=1 environment variable to work as expected for the test suite.
 def fake_time(time)
   old_time = ENV["FAKETIME"]
   new_time = time.strftime("%Y-%m-%d %H:%M:%S")
