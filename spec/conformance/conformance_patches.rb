b78b48053c7b8422b013d8df6dee037ed469132d
Gonzalo <grzuy0@gmail.com>
2020-02-03 15:03:45 -0300

    test: patch conformance server to fix TPM name test

--- spec/conformance/conformance_patches.rb
+++ spec/conformance/conformance_patches.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
-require "tpm/constants"
+require "tpm/key_attestation"
 
 ::TPM.send(:remove_const, "VENDOR_IDS")
 ::TPM::VENDOR_IDS = { "id:FFFFF1D0" => "FIDO Alliance" }.freeze
+
+module TPM
+  class Tpm2bName < BinData::Record
+    # Needed to workaround https://github.com/fido-alliance/conformance-tools-issues/issues/396
+    def valid_for?(object)
+      name.digest == OpenSSL::Digest.digest(TPM::TPM_TO_OPENSSL_HASH_ALG[name.hash_alg], object)
+    end
+  end
+end
