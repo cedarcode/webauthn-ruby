6667b153851258e64737afc6eb6a7952d8bc7e79
Nick Pezza <pezza@hey.com>
2024-10-23 16:35:07 -0400

    fix cops

--- lib/webauthn/camelize.rb
+++ lib/webauthn/camelize.rb
@@ -1,19 +1,23 @@
-module WebAuthn::Camelize
-  def deep_camelize_keys(object)
-    if object.is_a?(Hash)
-      object.each_with_object(Hash.new) do |(key, value), result|
-        result[camelize(key)] = deep_camelize_keys(value)
-      end
-    else
-      object
-    end
-  end
+# frozen_string_literal: true
 
-  def camelize(term)
-    term.to_s.
-      sub(/^(?:(?-mix:(?=a)b)(?=\b|[A-Z_])|\w)/) { |match| match.downcase }.
-      gsub(/(?:_|(\/))([a-z\d]*)/i) do
+module WebAuthn
+  module Camelize
+    def deep_camelize_keys(object)
+      if object.is_a?(Hash)
+        object.each_with_object({}) do |(key, value), result|
+          result[camelize(key)] = deep_camelize_keys(value)
+        end
+      else
+        object
+      end
+    end
+
+    def camelize(term)
+      term.to_s
+          .sub(/^(?:(?-mix:(?=a)b)(?=\b|[A-Z_])|\w)/) { |match| match.downcase }
+          .gsub(/(?:_|(\/))([a-z\d]*)/i) do
         $2.capitalize
       end.to_sym
+    end
   end
 end
