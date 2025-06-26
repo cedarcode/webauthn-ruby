7980dc00c732cf170a698684d951fbfb6d2ba3a5
Santiago Rodriguez <46354312+santiagorodriguez96@users.noreply.github.com>
2025-06-26 16:11:08 -0300

    test: update specs

--- spec/webauthn/u2f_migrator_spec.rb
+++ spec/webauthn/u2f_migrator_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe WebAuthn::U2fMigrator do
   end
 
   let(:stored_credential) { seeds[:u2f_migration][:stored_credential] }
-  let(:app_id) { URI("https://f69df4d9.ngrok.io") }
+  let(:app_id) { URI("https://example.org") }
 
   it "returns the credential ID" do
     expect(WebAuthn::Encoders::Base64Encoder.encode(u2f_migrator.credential.id))
-      .to eq("1a9tIwwYiYNdmfmxVaksOkxKapK2HtDNSsL4MssbCHILhkMzA0xZYk5IHmBljyblTQ/SnsQea+QEMzgTN2L1Mw==")
+      .to eq("pLpuLSz+xDZI19JcXtVlm8GPK3gVOFJ+vUkt4DJWvfQ=")
   end
 
   it "returns the credential public key in COSE format" do
@@ -30,8 +30,8 @@ RSpec.describe WebAuthn::U2fMigrator do
 
     expect(public_key.alg).to eq(-7)
     expect(public_key.crv).to eq(1)
-    expect(public_key.x).to eq(WebAuthn::Encoders::Base64Encoder.decode("FtOd9t3mxj6sLFkNCLzv5qS9l52MipHznrsZ+uwtHQY="))
-    expect(public_key.y).to eq(WebAuthn::Encoders::Base64Encoder.decode("np4zBpD5zhdSq1wKPvhzEoKJvFuYel1cpdTCzpahrBA="))
+    expect(public_key.x).to eq(WebAuthn::Encoders::Base64Encoder.decode("sNYt5rMPhvC6x6kBaVE5HC4xhJ4uZGYcvSsTzX1VCK0="))
+    expect(public_key.y).to eq(WebAuthn::Encoders::Base64Encoder.decode("UDsL2io1eppLNEdaKOZbZgtImKnj6bvwgg1DSUKX7dA="))
   end
 
   it "returns the signature counter" do
@@ -45,7 +45,7 @@ RSpec.describe WebAuthn::U2fMigrator do
   it "returns the attestation certificate" do
     certificate = u2f_migrator.attestation_trust_path.first
 
-    expect(certificate.subject.to_s).to eq("/CN=U2F Device")
-    expect(certificate.issuer.to_s).to eq("/CN=U2F Issuer")
+    expect(certificate.subject.to_s).to eq("/CN=WebAuthn test vectors/O=W3C/OU=Authenticator Attestation/C=AA")
+    expect(certificate.issuer.to_s).to eq("/CN=WebAuthn test vectors/O=W3C/OU=Authenticator Attestation CA/C=AA")
   end
 end
